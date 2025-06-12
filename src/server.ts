cc609241660d9550d92aa98bc2abf295402a89a6
landienzla <landienzla@gmail.com>
2025-06-12 15:01:23 +0300

    websocket

--- src/server.ts
+++ src/server.ts
@@ -1,85 +1,91 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import helmet from '@fastify/helmet'
-import rateLimit from '@fastify/rate-limit'
-import swagger from '@fastify/swagger'
-import swaggerUi from '@fastify/swagger-ui'
-import metrics from 'fastify-metrics'
-import connectDB from '../db'
-import { env, rateLimitOptions } from './config'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import rateLimit from "@fastify/rate-limit";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+import metrics from "fastify-metrics";
+import connectDB from "../db";
+import websocket from "@fastify/websocket";
+import { env, rateLimitOptions } from "./config";
 
 // Connect to MongoDB
-connectDB()
+connectDB();
 
 const app = Fastify({
-    logger: {
-        transport: {
-            target: 'pino-pretty',
-            options: {
-                colorize: true,
-                translateTime: 'HH:MM:ss Z',
-                ignore: 'pid,hostname',
-                singleLine: false,
-                messageFormat: '{msg}'
-            }
-        },
-        level: 'info'
-    }
-})
-
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+        singleLine: false,
+        messageFormat: "{msg}",
+      },
+    },
+    level: "info",
+  },
+});
+app.register(websocket);
+app.register(async function (fastify) {
+  fastify.get("/ws", { websocket: true }, function wsHandler(socket, req) {
+    socket.on("message", (message: string) => {
+      // message.toString() === 'hi from client'
+      socket.send("hi from server");
+    });
+  });
+});
 
 // Register core plugins
-app.register(cors)
-app.register(helmet)
+app.register(cors);
+app.register(helmet);
 app.register(rateLimit, {
-    ...rateLimitOptions
-})
+  ...rateLimitOptions,
+});
 
 app.register(metrics, {
-    endpoint: '/metrics'
-})
+  endpoint: "/metrics",
+});
 
 app.register(swagger, {
-    swagger: {
-        info: {
-            title: 'Fastify Boilerplate',
-            description: 'API documentation with Swagger UI',
-            version: '1.0.0'
-        },
-        externalDocs: {
-            url: 'https://github.com/landienzla/fastify-boilerplate',
-            description: 'Find more info here'
-        },
-        tags: [
-        ],
-        consumes: ['application/json'],
-        produces: ['application/json']
-    }
-})
-app.register(swaggerUi, {
-    routePrefix: '/docs',
-    uiConfig: {
-        docExpansion: 'full',
-        deepLinking: false
+  swagger: {
+    info: {
+      title: "Fastify Boilerplate",
+      description: "API documentation with Swagger UI",
+      version: "1.0.0",
     },
-    staticCSP: true,
-    transformStaticCSP: (header) => header,
-})
-
+    externalDocs: {
+      url: "https://github.com/landienzla/fastify-boilerplate",
+      description: "Find more info here",
+    },
+    tags: [],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+  },
+});
+app.register(swaggerUi, {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+});
 
 // Register routes
-import healthRoute from './routes/health'
-app.register(healthRoute, { prefix: '/health' })
-
+import healthRoute from "./routes/health";
+app.register(healthRoute, { prefix: "/health" });
 
 // Start the server
 const start = async () => {
-    try {
-        await app.listen({ port: Number(env.PORT) || 3000, host: '0.0.0.0' })
-    } catch (err) {
-        app.log.error(err)
-        process.exit(1)
-    }
-}
+  try {
+    await app.listen({ port: Number(env.PORT) || 3000, host: "0.0.0.0" });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
 
-start()
+start();
